d1ff6b033713888f62f7fca5e8b71c6f22c6e29f
Tomek <java-tomek@users.noreply.github.com>
2017-08-06 11:16:20 +0200

    add absolute value method

--- src/main/java/TheAbsoluteValue.java
+++ src/main/java/TheAbsoluteValue.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 
 public class TheAbsoluteValue {
+
+    private static int getAbsoluteValue(int number) {
+        if (number > 0) {
+            return number;
+        } else {
+            return -number;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj x");
         int x = scanner.nextInt();
-
-        if (x > 0)
-            System.out.println("Wartosc bezwzgledna: " + x);
-        else
-            System.out.println("Wartosc bezwzgledna: " + -x);
+        System.out.println("Wartosc bezwzgledna: " + getAbsoluteValue(x));
     }
 }
