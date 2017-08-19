be5117a87d856b51ccdff6490da06f5dfb46e89c
Tomek <java-tomek@users.noreply.github.com>
2017-08-19 11:50:10 +0200

    + eratosthenes on int[]

--- src/main/java/Eratosthenes.java
+++ src/main/java/Eratosthenes.java
@@ -1,6 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Eratosthenes {
+    private static int[] sieveOfEratosthenes2(int[] array) {
+        for (int i = 0; i < Math.floor(Math.sqrt(array.length)); i++) {
+            int numberToCompare = array[i];
+            if (numberToCompare != 0) {
+                for (int j = i + 1; j < array.length; j++) {
+                    if (array[j] % numberToCompare == 0) {
+                        array[j] = 0;
+                    }
+                }
+            }
+        }
+
+        return array;
+    }
+
+
     private static ArrayList<Integer> sieveOfEratosthenes(ArrayList<Integer> arrayList) {
         for (int i = 0; i < Math.floor(Math.sqrt(arrayList.size())); i++) {
             int numberToCompare = arrayList.get(i);
@@ -15,12 +32,16 @@ public class Eratosthenes {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        int[] array = new int[29];
         for (int i = 0; i < 29; i++) {
-            array.add(i + 2);
+            arrayList.add(i + 2);
+            array[i] = i + 2;
         }
 
-        System.out.println(array);
-        System.out.println(sieveOfEratosthenes(array));
+        System.out.println(arrayList);
+        System.out.println(sieveOfEratosthenes(arrayList));
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(sieveOfEratosthenes2(array)));
     }
 }
