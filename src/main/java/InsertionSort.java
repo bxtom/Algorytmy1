752a52e43c53295683b175ff739d95e27871a0ea
Tomek <java-tomek@users.noreply.github.com>
2017-08-19 11:29:48 +0200

    * insertion sort added break

--- src/main/java/InsertionSort.java
+++ src/main/java/InsertionSort.java
@@ -10,10 +10,48 @@ public class InsertionSort {
                         int temp = array[j];
                         array[j] = array[j - 1];
                         array[j - 1] = temp;
-                    }
+                    } else break;
                 }
             }
         }
+
+        return array;
+    }
+
+    private static int[] insertionSortDamian(int[] array) {
+        int[] result = new int[array.length];
+
+        if (array.length > 0) {
+            result[0] = array[0];
+        }
+
+        if (array.length > 1) {
+            for (int i = 1; i < array.length; i++) {
+                boolean added = false;
+
+                for (int j = 0; j < result.length; j++) {
+                    if (array[i] < result[j]) {
+                        result = move(result, array[i], j);
+                        added = true;
+                        break;
+                    }
+                }
+
+                if(added) {
+                    result[i] = array[i];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] move(int[] array, int insertValue, int insertIndex) {
+        for (int i = array.length - 1; i > insertIndex; i--) {
+            array[i] = array[i-1];
+        }
+
+        array[insertIndex] = insertValue;
 
         return array;
     }
@@ -27,5 +65,6 @@ public class InsertionSort {
 
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(insertionSort(array)));
+        System.out.println(Arrays.toString(insertionSortDamian(array)));
     }
 }
