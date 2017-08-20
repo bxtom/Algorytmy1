24c881858370544ee26e0d496838cf5cf652f595
Tomek <java-tomek@users.noreply.github.com>
2017-08-20 14:37:49 +0200

    * graph

---
+++ src/main/java/Graphs/GraphGnk.java
@@ -0,0 +1,11 @@
+package Graphs;
+
+public class GraphGnk {
+    public static Edge[] generateGnk(int n, int k) {
+        Edge[] result = Structures.generateEdgeArray(n);
+
+
+
+        return result;
+    }
+}
