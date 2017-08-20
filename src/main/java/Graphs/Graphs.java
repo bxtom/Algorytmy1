75ed687df3d13a1fd5d0e7535aec48537d028540
Tomek <java-tomek@users.noreply.github.com>
2017-08-20 14:53:05 +0200

    * graph

--- src/main/java/Graphs/Graphs.java
+++ src/main/java/Graphs/Graphs.java
@@ -2,7 +2,8 @@ package Graphs;
 
 public class Graphs {
     public static void main(String[] args) {
-        generateGraph(4, 0.5);
+        //generateGraph(4, 0.5);
+        GraphGnk.generateGnk(4, 6);
     }
 
     public static void generateGraph(int numberOfVertices, double probability) {
