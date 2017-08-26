a9a629976a2ea06bb99c42053ecd5c0614df7332
Tomek <java-tomek@users.noreply.github.com>
2017-08-26 11:40:34 +0200

    + heap on array

--- src/main/java/Heap/HeapOnArray.java
+++ src/main/java/Heap/HeapOnArray.java
@@ -27,6 +27,45 @@ class HeapOnArray {
         return 0;
     }
 
+    public void quickCleanHeap() {
+        this.currentIndex = 0;
+    }
+
+    public void cleanHeap() {
+        for (int i = 0; i < this.heap.length; i++) {
+            this.heap[i] = 0;
+        }
+        quickCleanHeap();
+    }
+
+    public void removeByValue(int value) {
+        int[] temp = new int[this.heap.length];
+
+        int tempIndex = 0;
+        for (int i = 0; i < this.heap.length; i++) {
+            if (this.heap[i] != value) {
+                temp[tempIndex] = this.heap[i];
+                tempIndex++;
+            } else {
+                this.currentIndex--;
+            }
+
+        }
+
+        this.heap = temp;
+    }
+
+    public void removeByValueReplace(int value) {
+        for (int i = 0; i < this.heap.length; i++) {
+            if (this.heap[i] == value) {
+                for (int j = i + 1; j < this.heap.length; j++) {
+                    this.heap[j - 1] = this.heap[j];
+                }
+                this.currentIndex--;
+            }
+        }
+    }
+
     int getPeakOfHeap() {
         return this.heap[this.currentIndex - 1];
     }
