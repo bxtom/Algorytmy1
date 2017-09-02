7ffc0bb02bc3d377b07f345c0c7152ae8e697e85
Tomek <java-tomek@users.noreply.github.com>
2017-09-02 13:34:51 +0200

    + vector matrix

---
+++ src/main/java/VectorMatrix.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class VectorMatrix {
+    private static final int N = 10;
+    private static final int BOUND = 10;
+
+    private static int[] multiply(int[][] matrix, int[] vector) {
+        int[] result = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                result[i] += vector[j] * matrix[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    private static int[] multiplyUsingStreams(int[][] matrix, int[] vector) {
+        return Arrays.stream(matrix)
+                .mapToInt(row ->
+                        IntStream.range(0, row.length)
+                                .map(col -> row[col] * vector[col])
+                                .sum()
+                ).toArray();
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] vector = new int[N];
+        for (int i = 0; i < N; i++) {
+            vector[i] = random.nextInt(BOUND);
+        }
+
+        int[][] matrix = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = random.nextInt(BOUND);
+            }
+        }
+
+        System.out.println(Arrays.toString(vector));
+        System.out.println("");
+        Arrays.stream(matrix).forEach(array -> System.out.println(Arrays.toString(array)));
+        System.out.println("");
+        System.out.println(Arrays.toString(multiply(matrix, vector)));
+        System.out.println("");
+        System.out.println(Arrays.toString(multiplyUsingStreams(matrix, vector)));
+    }
+}
