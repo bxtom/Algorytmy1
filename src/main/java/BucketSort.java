f5045b333b10f19e3ef07311c4184966c55b044c
Tomek <java-tomek@users.noreply.github.com>
2017-09-02 13:35:03 +0200

    + bucket sort cd

--- src/main/java/BucketSort.java
+++ src/main/java/BucketSort.java
@@ -4,6 +4,8 @@ import java.util.stream.IntStream;
 
 public class BucketSort {
 
+    private static int[] denominations = {1, 2, 5, 10, 20, 50, 100, 200, 500};
+
     private static String printSortedArray(int[] array) {
         StringBuilder result = new StringBuilder();
 
@@ -34,5 +36,16 @@ public class BucketSort {
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(bucketSort(array)));
         System.out.println(printSortedArray(bucketSort(array)));
+
+
+        int[] cash = new int[10];
+        for (int i = 0; i < cash.length; i++) {
+            cash[i] = denominations[random.nextInt(denominations.length)];
+        }
+
+        System.out.println("");
+        System.out.println(Arrays.toString(cash));
+
+        int[] cashBuckets = new int[9];
     }
 }
