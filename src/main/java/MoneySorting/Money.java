ecd430d7760340ffa967b26feed224a74ef334c5
Tomek <java-tomek@users.noreply.github.com>
2017-09-03 11:06:26 +0200

    + money sorting

--- src/main/java/MoneySorting/Money.java
+++ src/main/java/MoneySorting/Money.java
@@ -1,25 +1,27 @@
 package MoneySorting;
 
+import java.math.BigDecimal;
+
 public class Money {
-    private double value;
+    private BigDecimal value;
     private Currency currency;
 
-    public Money(double value, Currency currency) {
+    public Money(BigDecimal value, Currency currency) {
         this.value = value;
         this.currency = currency;
     }
 
-    public double getOriginalValue() {
+    public BigDecimal getOriginalValue() {
         return value;
     }
 
-    public double getValue() {
+    public BigDecimal getValue() {
         switch (this.currency) {
             case EUR:
-                this.value *= 4.2;
+                this.value = this.value.multiply(BigDecimal.valueOf(4.2));
                 break;
             case USD:
-                this.value *= 3.5;
+                this.value = this.value.multiply(BigDecimal.valueOf(3.5));
                 break;
         }
 
--- src/main/java/MoneySorting/MoneySorting.java
+++ src/main/java/MoneySorting/MoneySorting.java
@@ -1,7 +1,19 @@
 package MoneySorting;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 public class MoneySorting {
     public static void main(String[] args) {
+        Money[] moneyArray = new Money[5];
+        BigDecimal test = new BigDecimal(5);
+        moneyArray[0] = new Money(BigDecimal.valueOf(5), Currency.EUR);
+        moneyArray[1] = new Money(BigDecimal.valueOf(4), Currency.PLN);
+        moneyArray[2] = new Money(BigDecimal.valueOf(6), Currency.USD);
+        moneyArray[3] = new Money(BigDecimal.valueOf(9), Currency.PLN);
+        moneyArray[4] = new Money(BigDecimal.valueOf(11), Currency.EUR);
+
+        System.out.println(Arrays.toString(moneyArray));
 
     }
 }
